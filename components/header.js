2fc838cb2417c626ea979098e3a8093c41d6a3c2
Francis Agulto <francis.agulto@wpengine.com>
2021-11-04 15:01:01 -0500

    initial version

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <>
+      <Image src="/download.jpg" alt="JamStack" width={100} height={100} />
+    </>
+  );
+}
